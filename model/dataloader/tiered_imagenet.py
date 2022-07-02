1b5f117d66fc3ed8359a1c8df2bb9b9aef2045bd
shule-deng <108419031+shule-deng@users.noreply.github.com>
2022-07-02 14:23:38 +0800

    Add files via upload

---
+++ model/dataloader/tiered_imagenet.py
@@ -0,0 +1,243 @@
+from __future__ import print_function
+
+import os
+import os.path as osp
+import numpy as np
+import pickle
+import sys
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from PIL import Image
+
+# Set the appropriate paths of the datasets here.
+THIS_PATH = osp.dirname(__file__)
+ROOT_PATH1 = osp.abspath(osp.join(THIS_PATH, '..', '..', '..'))
+ROOT_PATH2 = osp.abspath(osp.join(THIS_PATH, '..', '..'))
+#IMAGE_PATH = osp.join(ROOT_PATH2, 'data/tiered-imagenet/')
+#SPLIT_PATH = osp.join(ROOT_PATH2, 'data/tiered-imagenet/split')
+
+def buildLabelIndex(labels):
+    label2inds = {}
+    for idx, label in enumerate(labels):
+        if label not in label2inds:
+            label2inds[label] = []
+        label2inds[label].append(idx)
+
+    return label2inds
+
+
+def load_data(file):
+    try:
+        with open(file, 'rb') as fo:
+            data = pickle.load(fo)
+        return data
+    except:
+        with open(file, 'rb') as f:
+            u = pickle._Unpickler(f)
+            u.encoding = 'latin1'
+            data = u.load()
+        return data
+
+
+class CategoriesSampler():
+
+    def __init__(self, label, n_batch, n_cls, n_per):
+        self.n_batch = n_batch
+        self.n_cls = n_cls
+        self.n_per = n_per
+
+        label = np.array(label)
+        self.m_ind = []
+        for i in range(max(label) + 1):
+            ind = np.argwhere(label == i).reshape(-1)
+            ind = torch.from_numpy(ind)
+            self.m_ind.append(ind)
+
+    def __len__(self):
+        return self.n_batch
+
+    def __iter__(self):
+        for i_batch in range(self.n_batch):
+            batch = []
+            classes = torch.randperm(len(self.m_ind))[:self.n_cls]
+            for c in classes:
+                l = self.m_ind[c]
+                pos = torch.randperm(len(l))[:self.n_per]
+                batch.append(l[pos])
+            batch = torch.stack(batch).t().reshape(-1)
+            yield batch
+
+class tieredImageNet(data.Dataset):
+    def __init__(self, setname, args, augment=False):
+        assert(setname=='train' or setname=='val' or setname=='test')
+        
+        IMAGE_PATH = args.data_path
+        
+        file_path = {'train':[os.path.join(IMAGE_PATH, 'train_images.npz'), os.path.join(IMAGE_PATH, 'train_labels.pkl')],
+                     'val':[os.path.join(IMAGE_PATH, 'val_images.npz'), os.path.join(IMAGE_PATH,'val_labels.pkl')],
+                     'test':[os.path.join(IMAGE_PATH, 'test_images.npz'), os.path.join(IMAGE_PATH, 'test_labels.pkl')]}
+
+        
+        image_path = file_path[setname][0]
+        label_path = file_path[setname][1]
+
+        data_train = load_data(label_path)
+        labels = data_train['labels']
+        self.data = np.load(image_path)['images']
+        label = []
+        lb = -1
+        self.wnids = []
+        for wnid in labels:
+            if wnid not in self.wnids:
+                self.wnids.append(wnid)
+                lb += 1
+            label.append(lb)
+
+        self.label = label
+        self.num_class = len(set(label))
+        print(self.num_class)
+        if augment and setname == 'train':
+            transforms_list = [
+                  transforms.RandomCrop(84, padding=8),
+                  transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+                  transforms.RandomHorizontalFlip(),
+                  transforms.ToTensor(),
+                ]
+        else:
+            resize = int(84*256./224.)#####################
+            transforms_list = [
+                  transforms.Resize(resize),#####################
+                  transforms.CenterCrop(84),#####################
+                  transforms.ToTensor(),
+                ]
+
+        # Transformation
+        if args.backbone_class == 'ConvNet':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(np.array([0.485, 0.456, 0.406]),
+                                     np.array([0.229, 0.224, 0.225]))
+            ])
+        elif args.backbone_class == 'ResNet':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]),
+                                     np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))
+            ])
+        elif args.backbone_class == 'Res12':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(np.array([x / 255.0 for x in [120.39586422,  115.59361427, 104.54012653]]),
+                                     np.array([x / 255.0 for x in [70.68188272,   68.27635443,  72.54505529]]))
+            ])
+        elif args.backbone_class == 'Res18':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])    
+        elif args.backbone_class == 'WRN':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])                   
+        else:
+            raise ValueError('Non-supported Network Types. Please Revise Data Pre-Processing Scripts.')
+
+
+    def __getitem__(self, index):
+        img, label = self.data[index], self.label[index]
+        img = self.transform(Image.fromarray(img))
+        return img, label
+
+    def __len__(self):
+        return len(self.data)
+
+
+
+
+'''from __future__ import print_function
+
+import os
+import os.path as osp
+import numpy as np
+import pickle
+import sys
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from PIL import Image
+
+
+class tieredImageNet(data.Dataset):
+
+    def __init__(self, setname, args, augment=False):
+        split_dir = args.split_path
+        split_file = os.path.join(split_dir, setname + '.csv')
+        assert os.path.isfile(split_file)
+        with open(split_file, 'r') as f:
+            split = [x.strip().split(',') for x in f.readlines()[1:] if x.strip() != '']
+
+        data, ori_labels = [x[0] for x in split], [x[1] for x in split]
+        label_key = sorted(np.unique(np.array(ori_labels)))
+        label_map = dict(zip(label_key, range(len(label_key))))
+        mapped_labels = [label_map[x] for x in ori_labels]
+        
+        if augment and setname == 'train':
+            transforms_list = [
+                  transforms.RandomResizedCrop(84),
+                  transforms.RandomHorizontalFlip(),
+                  transforms.ToTensor(),
+                ]
+        else:
+            transforms_list = [
+                  transforms.ToTensor(),
+                ]
+
+        # Transformation
+        if args.backbone_class == 'ConvNet':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(np.array([0.485, 0.456, 0.406]),
+                                     np.array([0.229, 0.224, 0.225]))
+            ])
+        elif args.backbone_class == 'ResNet':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]),
+                                     np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))
+            ])
+        elif args.backbone_class == 'Res12':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(np.array([x / 255.0 for x in [120.39586422,  115.59361427, 104.54012653]]),
+                                     np.array([x / 255.0 for x in [70.68188272,   68.27635443,  72.54505529]]))
+            ])
+        elif args.backbone_class == 'Res18':
+            self.transform = transforms.Compose(
+                transforms_list + [
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])                      
+        else:
+            raise ValueError('Non-supported Network Types. Please Revise Data Pre-Processing Scripts.')
+
+        self.root = osp.join(args.data_path, 'data')
+        self.data = data
+        self.label = mapped_labels
+        self.num_class = len(set(self.label))
+        self.length = len(self.data)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        assert os.path.isfile(self.root+'/'+self.data[index])
+        img = Image.open(self.root + '/' + self.data[index]).convert('RGB')
+        label = self.label[index]
+        label = int(label)
+        if self.transform:
+            img = self.transform(img)
+        return img, label'''
